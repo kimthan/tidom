9672139fec63262aef3e4ffef31994aed9e7fa40
kimthan <kimthan85@gmail.com>
2018-08-12 10:31:26 +0200

    added pics

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { Slide  } from 'react-slideshow-image';
 import './Showcase.css'
-class showcase extends Component {
+
+const images = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
     
+    
+  ];
+
+class showcase extends Component {
+
+
+
+
     render() {
         return (
             <header id="showcase" className="grid">
-            <div className="bg-image">
-                <img src="img/showcase_desktop.jpeg" />
-            </div>
+             <Slide
+          images={images}
+          duration={1000}
+          transitionDuration={1000}
+        />
             <h1>Välkommen till</h1>
             <h1>King's Salong</h1>
+            
           </header>
         );
     }
